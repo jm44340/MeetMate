cc5e7508b2931322fde37a1899365c206a27317e
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-04-26 11:06:32 +0200

    sms provider

--- meetmate/sms_provider.py
+++ meetmate/sms_provider.py
@@ -16,3 +16,18 @@ def send_sms(recipient, message):
 		"sandbox": "1"  # TODO set to 0 to disable sandbox
 	}
 	requests.post(url=url, data=data)
+
+
+def send_2fa(recipient, code):
+	url = setting["sms_url"]
+	message = "Twoje jednorazowe haslo SMS do autoryzacji: " + code
+	data = {
+		"login": setting["sms_user"],
+		"pass": setting["sms_pass"],
+		"msg_type": setting["sms_type"],
+		"recipient": recipient,
+		"message": message,
+		"sandbox": "1"  # TODO set to 0 to disable sandbox
+	}
+	requests.post(url=url, data=data)
+	print(message)
