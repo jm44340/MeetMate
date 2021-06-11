0cefa1d584c36606aabf2550f32ae93d13536611
jm44340 <jm44340@zut.edu.pl>
2021-06-11 19:20:41 +0200

    Refactoring meet_qr_check

--- meetmate/routes/user.py
+++ meetmate/routes/user.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import json
 
 from flask import render_template, redirect, url_for, session, request
 from meetmate import app
@@ -7,7 +8,6 @@ from routes import error
 import Meet
 import Presence
 import User
-
 
 @app.route("/user")
 def user_panel():
@@ -45,39 +45,14 @@ def scan_qr():
         return redirect(url_for("login"))
     return render_template("scan_qr.html")
 
-
-# @app.route("/meet/<meet_id>/<qr_hash>")
-# def meet_qr_check(meet_id, qr_hash):
-#     if "user" not in session.keys():
-#         return redirect(url_for("login"))
-#
-#     user = User.User(session["user"])
-#     if user.type != User.UserType.USER:
-#         return redirect(url_for("login"))
-#
-#     try:
-#         meet = Meet.Meet.get_by_linkid(meet_id)
-#     except Meet.ExistError:
-#         return error.error("meet-not-exist")
-#
-#     if meet.status != Meet.MeetStatus.ACTIVE:
-#         return error.error("meet-not-active")
-#
-#     if not meet.check_qr_data(qr_hash):
-#         return error.error("qr-not-valid")
-#
-#     presence = Presence.Presence.new_presence(meet, user)
-#     return "OK"
-
-
-@app.route("/meet/<meet_id>/<qr_hash>")
+@app.route("/meet/<meet_id>/<qr_hash>", methods=['GET', 'POST'])
 def meet_qr_check(meet_id, qr_hash):
     if "user" not in session.keys():
         return redirect(url_for("login"))
 
     user = User.User(session["user"])
     if user.type != User.UserType.USER:
-        return redirect(url_for("login"))
+        return redirect(url_for("index"))
 
     try:
         meet = Meet.Meet.get_by_linkid(meet_id)
@@ -88,73 +63,43 @@ def meet_qr_check(meet_id, qr_hash):
         return error.error("meet-not-active")
 
     if not meet.check_qr_data(qr_hash):
-        return error.error("qr-not-valid")
-
-    session["meet_id"] = meet_id
-    return redirect(url_for('meet_qr_confirm'))
-
-
-# @app.route("/confirm/<meet_id>/<secret>", methods=['GET', 'POST'])
-# def meet_qr_confirm(meet_id, secret):
-#     if request.method == 'GET':
-#         if "secret" not in session.keys():
-#             return redirect(url_for("index"))
-#         if secret != session["secret"]:
-#             return redirect(url_for("index"))
-#         session.pop("secret", None)
-#
-#         return render_template("confirm.html", meet=meet_id)
-#
-#
-#     if request.method == 'POST':
-#         print("POST")
-#         gps = request.form.get('gps')
-#         print(gps)
-#         return "OK"
-#
-
-
-    #
-
-    #
-    # try:
-    #     meet = Meet.Meet.get_by_linkid(meet_id)
-    # except Meet.ExistError:
-    #     return error.error("meet-not-exist")
-    #
-    # if meet.status != Meet.MeetStatus.ACTIVE:
-    #     return error.error("meet-not-active")
-    #
-    # if not meet.check_qr_data(qr_hash):
-    #     return error.error("qr-not-valid")
-    #
-    # presence = Presence.Presence.new_presence(meet, user)
-
-
-
-
-
-
-@app.route("/confirm", methods=['GET', 'POST'])
-def meet_qr_confirm():
-    user = User.User(session["user"])
-    if user.type != User.UserType.USER:
-        return redirect(url_for("login"))
-    if request.method == 'GET':
-        if "meet_id" not in session.keys():
-            return redirect(url_for("index"))
-        meet_id = session["meet_id"]
-        return render_template("confirm.html", meet=meet_id)
+         return error.error("qr-not-valid")
 
     if request.method == 'POST':
-        latitude = request.form.get('latitude')
-        longitude = request.form.get('longitude')
-        meet_id = session["meet_id"]
-        session.pop("meet_id", None)
-        meet = Meet.Meet.get_by_linkid(meet_id)
-        if not meet.check_localization(float(longitude), float(latitude)):
-            return "ERR"
-        presence = Presence.Presence.new_presence(meet, user)
-        return "OK"
+        try:
+            latitude = request.form.get('latitude')
+            longitude = request.form.get('longitude')
+        except:
+            return json.dumps({"status": "ERROR"})
 
-    return redirect(url_for('index'))
+        if not meet.check_localization(float(longitude), float(latitude)):
+            return json.dumps({"status": "TOO FAR"})
+
+        Presence.Presence.new_presence(meet, user)
+        return json.dumps({"status": "OK"})
+
+    return render_template("confirm.html")
+
+# @app.route("/confirm", methods=['GET', 'POST'])
+# def meet_qr_confirm():
+#     user = User.User(session["user"])
+#     if user.type != User.UserType.USER:
+#         return redirect(url_for("login"))
+#     if request.method == 'GET':
+#         if "meet_id" not in session.keys():
+#             return redirect(url_for("index"))
+#         meet_id = session["meet_id"]
+#         return render_template("confirm.html", meet=meet_id)
+
+#     if request.method == 'POST':
+#         latitude = request.form.get('latitude')
+#         longitude = request.form.get('longitude')
+#         meet_id = session["meet_id"]
+#         session.pop("meet_id", None)
+#         meet = Meet.Meet.get_by_linkid(meet_id)
+#         if not meet.check_localization(float(longitude), float(latitude)):
+#             return "ERR"
+#         presence = Presence.Presence.new_presence(meet, user)
+#         return "OK"
+
+#     return redirect(url_for('index'))
