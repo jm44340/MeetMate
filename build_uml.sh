3c610d0f6991e83ec965d40eb9162d927e4d57a2
jm44340 <jm44340@zut.edu.pl>
2021-06-13 23:28:34 +0200

    fix build_uml.sh

--- build_uml.sh
+++ build_uml.sh
@@ -9,8 +9,7 @@ fi
 pushd ./meetmate
 pyreverse -A -o pdf -p meetmate \
   ./*.py \
-  ./Widgets/* \
-  ./AlgorithmsLogic/*
+  ./routes/*
 mv classes_meetmate.pdf ..
 mv packages_meetmate.pdf ..
 popd
